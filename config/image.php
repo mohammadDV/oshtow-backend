eeb6eedd31a31bbdf799c5933487ae2f65bfb948
Mohammad daneshmand <81333964+mohammadDV@users.noreply.github.com>
2025-08-06 20:39:51 +0300

    fix an issue

--- config/image.php
+++ config/image.php
@@ -38,7 +38,7 @@ return [
     // 'default-profile-image' => 'https://prod-data-sport.storage.iran.liara.space/oshtow/default-avatar.jpg',
     'default-profile-image' => 'oshtow/uploads/images/default/user-3/2025/08/05/5oDhYkWyG3iywqqsoP2Io35sASpnJzoHiEOlOn2l.jpg',
     // 'default-background-image' => 'https://prod-data-sport.storage.iran.liara.space/oshtow/default-user-banner.jpg',
-    'default-background-image' => 'oshtow/uploads/images/default/user-3/2025/08/05/94hFAge9cTsYZlAQ9aF0zLPt9QHsyHTfJ874SIZh.jpg"',
+    'default-background-image' => 'oshtow/uploads/images/default/user-3/2025/08/05/94hFAge9cTsYZlAQ9aF0zLPt9QHsyHTfJ874SIZh.jpg',
     // 'default_project_image' => 'https://prod-data-sport.storage.iran.liara.space/oshtow/default-destination1.jpeg',
     'default_project_image' => 'oshtow/uploads/images/default/user-3/2025/08/05/dTLrFqPILuXwcEHg9CNgpLpdoktV4LRxsJ1Xf5Tk.jpg',
 ];
